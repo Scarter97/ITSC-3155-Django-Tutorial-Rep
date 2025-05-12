aef541bfc3eb32931e4af738b74f4467994ec782
Stephon Carter <scarte97@uncc.edu>
2025-05-12 05:30:09 -0400

    User Login Commit

--- base/urls.py
+++ base/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('', views.home, name="home"),
     path('room/<str:pk>', views.room, name="room"),
 
     path('create-room/', views.createRoom, name="create-room"),
     path('update-room/<int:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<int:pk>/', views.deleteRoom, name="delete-room"),
+
+    path('login/', views.loginPage, name='login'),
 ]
